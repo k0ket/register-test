119a9a7f7a2009ecc611d63847d7bb0ce6f05750
Andrii Vilkhovskyi <dev694512@outlook.com>
2021-08-02 18:35:13 +0300

    Added Tests

---
+++ tests/Feature/Api/v1/AccountTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Tests\Feature\Api\v1;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class AccountTest extends TestCase
+{
+    use WithFaker;
+
+    public function test(): void
+    {
+        $response = $this->get(route('api.v1.admin.account'),
+            [
+                'sort'     => 'created_at',
+                'sortBy'   => 'desc',
+                'filter'   => 'phone_no1',
+                'filterBy' => '31231123125',
+                'page'     => '1',
+            ]);
+
+        $response
+            ->assertJson(fn(AssertableJson $json) => $json
+                ->has('data')
+                ->has('meta')
+                ->has('links')
+            )
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'name',
+                        'address1',
+                        'address2',
+                        'city',
+                        'state',
+                        'country',
+                        'zipCode',
+                        'latitude',
+                        'longitude',
+                        'phoneNo1',
+                        'phoneNo2',
+                        'totalUser',
+                        'startValidity',
+                        'endValidity',
+                        'status',
+                        'createdAt',
+                        'updatedAt',
+                    ],
+                ],
+            ]);
+    }
+}
---
+++ tests/Feature/Api/v1/RegisterTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Feature\Api\v1;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    use WithFaker;
+
+    public function test(): void
+    {
+        $email    = $this->faker->email;
+        $response = $this->post(route('api.v1.auth.register'), [
+            'user'       => [
+                'firstName'            => $this->faker->firstName,
+                'lastName'             => $this->faker->lastName,
+                'email'                => $email,
+                'password'             => 'password*123',
+                'passwordConfirmation' => 'password*123',
+                'phone'                => $this->faker->e164PhoneNumber,
+            ],
+            'clientName' => $this->faker->name,
+            'address1'   => $this->faker->streetAddress,
+            'address2'   => $this->faker->streetAddress,
+            'city'       => $this->faker->city,
+            'state'      => $this->faker->state,
+            'country'    => $this->faker->country,
+            'phoneNo1'   => $this->faker->e164PhoneNumber,
+            'phoneNo2'   => $this->faker->e164PhoneNumber,
+            'zipCode'    => $this->faker->postcode,
+        ]);
+
+        $response->assertOk();
+    }
+}
